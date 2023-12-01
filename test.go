178ab7969565242adcddc97388810ea3304b0109
Rob Conklin <robconklin@ripplescience.com>
2023-12-01 16:00:45 -0500

    added comments

--- test.go
+++ test.go
@@ -9,15 +9,22 @@ import (
 	"strings"
 )
 
+// Entry point
 func main() {
 	fmt.Println(addUpValuesFromInputFile())
 }
 
+// Adds up the individual digit components turning them into a 2-digit
+// number
 func getFirstAndLastAsTwoDigit(input string) int {
 	firstNum, lastNum := firstAndLast(input)
 	return firstNum*10 + lastNum
 }
 
+// firstAndLast accepts an input string and determines the first
+// and last integer values to appear in the string
+// either as single-digit integers or as words representing those single
+// digit integers.
 func firstAndLast(input string) (first int, last int) {
 	for pos := 0; pos < len(input); pos++ {
 		if input[pos] >= '0' && input[pos] <= '9' {
@@ -41,9 +48,15 @@ func firstAndLast(input string) (first int, last int) {
 	return
 }
 
+// getNumberWord accepts the input string and the current position to search
+// for a number "word", if the input string at that position starts with a
+// number word, it will return the int value of that word, or -1 if it does
+// not start with a number word.
 func getNumberWord(input string, pos int) int {
 	//get the current substring from our current position
 	restOfString := input[pos:]
+
+	//handle turning words into numbers
 	switch {
 	case strings.Index(restOfString, "one") == 0:
 		return 1
@@ -68,9 +81,14 @@ func getNumberWord(input string, pos int) int {
 	case strings.Index(restOfString, "two") == 0:
 		return 2
 	}
+
+	// flag value for no number found
 	return -1
 }
 
+// addUpValuesFromInputFile is responsible for reading the input.txt file and
+// parsing each line looking for the first and last numbers, totalling them
+// up and returning the sum from the entire file
 func addUpValuesFromInputFile() (total int) {
 	file, err := os.Open("input.txt")
 	if err != nil {
